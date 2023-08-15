6e21cfbe757732fbd55068d8f68f08515db1a878
JD2929 <jandunk@creonova.com>
2023-08-15 18:35:47 -0300

    Initial stages of setting up the functions.

---
+++ assets/questions.js
@@ -0,0 +1,18 @@
+var questions= [
+
+    {
+        question:"Inside which HTML element do we put the JavaScript?",
+        answers:["script","link", "join", "connect"], 
+        correct:"script"
+    },
+    {
+        question:"Where is the correct place to insert the JavaScript?",
+        answers:["body","head", "header", "footer"],
+        correct:"body"
+    },
+    {
+        question:"What does the word 'this' refer to in JavaScript?",
+        answers:["the object executing the code", "the method attached to the object", "the variable that is currently being used", "a new variable name"],
+        correct:"the object executing the code"
+    }
+]
